eb74523adec6ab0bd836fee5fffeb6817dd188af
Kad1711 <nguyenkhaduong17@gmail.com>
2026-03-26 00:26:51 +0700

    point admin to renenddeerr backend

--- spotify-clone-admin/src/services/api.js
+++ spotify-clone-admin/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api"
+  baseURL: "https://spotify-clone-backend-0t69.onrender.com/api"
 });
 
 export default api;
