02c276e2fe43ad95a059336a6eb5ce7d3b44a157
Kad1711 <nguyenkhaduong17@gmail.com>
2026-03-26 01:44:20 +0700

    tao api.js trong admin de deloy admin luon

--- SpotifyClone/app/src/main/java/com/example/spotifyclone/data/remote/ApiClient.kt
+++ SpotifyClone/app/src/main/java/com/example/spotifyclone/data/remote/ApiClient.kt
@@ -4,7 +4,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object ApiClient {
-    private const val BASE_URL = "http://10.0.2.2:5000/api/"
+    private const val BASE_URL = "https://spotify-clone-backend-0t69.onrender.com/api/"
 
     val apiService: SpotifyApiService by lazy {
         Retrofit.Builder()
