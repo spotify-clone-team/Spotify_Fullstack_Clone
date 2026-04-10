3b23d9899b31525b539e603b92093f318c6c6908
Nguyễn Khả Dương <nguyenkhaduong17@gmail.com>
2026-04-10 16:30:02 +0700

    fix giao dien sidebar admin

--- spotify-clone-admin/src/layouts/AdminLayout.jsx
+++ spotify-clone-admin/src/layouts/AdminLayout.jsx
@@ -4,7 +4,9 @@ import Sidebar from "../components/Sidebar";
 export default function AdminLayout() {
   return (
     <div className="admin-layout">
-      <Sidebar />
+      <div style={{ height: "100vh", position: "fixed", top: 0, left: 0 }}>
+        <Sidebar />
+      </div>
       <main className="admin-content">
         <Outlet />
       </main>
